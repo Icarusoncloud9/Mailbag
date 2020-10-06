8ac9bbb8dc6eebd3a4c7bca0f72e89678104e765
Joaquin Melara <hermes@Joaquins-MacBook-Pro.local>
2020-10-06 01:59:30 -0400

    Debugged code

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,37 +1,27 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-
   entry : "./src/code/main.tsx",
-
   resolve : {
     extensions : [ ".ts", ".tsx", ".js" ]
   },
-
   module : {
     rules : [
-      {
-        test : /\.html$/,
+      { test : /\.html$/,
         use : { loader : "html-loader" }
       },
-      {
-        test : /\.css$/,
+      { test : /\.css$/,
         use : [ "style-loader", "css-loader"]
       },
-      {
-        test : /\.tsx?$/,
+      { test : /\.tsx?$/,
         loader: 'awesome-typescript-loader'
       }
     ]
-
   },
-
   plugins : [
     new HtmlWebPackPlugin({ template : "./src/index.html", filename : "./index.html" })
   ],
-
   performance : { hints : false },
   watch : true,
   devtool : "source-map"
-
 };
